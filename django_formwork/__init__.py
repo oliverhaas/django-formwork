97a813ec38c4f3ded604d6098d46da412549f460
Oliver Haas <ohaas@e1plus.de>
2026-04-19 21:06:20 +0200

    feat: export FormworkChoiceLabel and enriched choice fields

--- django_formwork/__init__.py
+++ django_formwork/__init__.py
@@ -6,12 +6,20 @@ except PackageNotFoundError:
     __version__ = "0.0.0+unknown"
 
 from django_formwork.async_forms import AsyncFormMixin, AsyncModelFormMixin
+from django_formwork.fields import (
+    FormworkChoiceLabel,
+    FormworkModelChoiceField,
+    FormworkModelMultipleChoiceField,
+)
 from django_formwork.renderers import FormworkJinja2Renderer, FormworkRenderer
 
 __all__ = [
     "AsyncFormMixin",
     "AsyncModelFormMixin",
+    "FormworkChoiceLabel",
     "FormworkJinja2Renderer",
+    "FormworkModelChoiceField",
+    "FormworkModelMultipleChoiceField",
     "FormworkRenderer",
     "__version__",
 ]
